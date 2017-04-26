e19b3404d7492a875f3fba223c82fba287e899a3
raon <raon_j@madeweb.co.kr>
2017-04-26 14:16:31 +0900

    2017-04-26 프로모션 페이지 수정 및 공동 페이징 수정

--- js/common.js
+++ js/common.js
@@ -210,7 +210,7 @@ function Rolling_transition(obj){
     reponsive 작성
     작성일 : 2017.04.26
     작성자 : 정진배
-    내용 : 서브페이지 기능
+    내용 : 프로모션페이지 아코디언 기능
 *********************************************************************************/
 $(function(){
     var promotionBtn = $(".promotion_box > a");
@@ -221,14 +221,15 @@ $(function(){
         e.preventDefault();
         if($(this).parent().hasClass("on")){
             $(this).parent().removeClass("on");
-            $(this).parent().next().slideUp("slow");
+            $(this).parent().next().removeClass("on");
+            $(this).parent().next().slideUp("2000");
             promotionArrow.attr("src", "../images/arrow/arrow02.png")
         } else {
             promotionBtn.parent().next().slideUp();
             promotionBtn.parent().removeClass("on");
             promotionArrow.attr("src", "../images/arrow/arrow02.png")
             $(this).parent().addClass("on");
-            $(this).parent().next().slideDown("slow");
+            $(this).parent().next().slideDown("50");
             $(this).find(".arrow").attr("src", "../images/arrow/arrow02_on.png")
         }
     })
