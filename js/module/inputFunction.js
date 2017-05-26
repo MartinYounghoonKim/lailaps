db8990148e7297923f55e1fca11dec209ee4be09
younghoon <martin.younghoon.kim@gmail.com>
2017-05-26 21:28:57 +0900

    2017-05-26 김영훈 input 기능 추가 작업

--- js/module/inputFunction.js
+++ js/module/inputFunction.js
@@ -1,5 +1,28 @@
 define([
     'jquery'
 ], function($){
-    console.log($);
+    var inputValidator = function(options){
+        this.wrapper = $(options.wrapper);
+    }
+    inputValidator.prototype = {
+        bindEvents:function(){
+            var that = this;
+            this.wrapper.on("focus",function(){
+                that.getCountVal($(this));
+            })
+            /*this.wrapper[0].onkeyup = function(e){
+                console.log(e.keyCode)
+            }*/
+        },
+        getCountVal:function(me){
+            var textType = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+            var countText = setInterval(function(){
+                console.log(11);
+                if(me.val().length >0){
+                    clearInterval(countText);
+                }
+            },100)
+        },
+    }
+    return inputValidator;
 })
