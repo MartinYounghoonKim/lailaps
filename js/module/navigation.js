dd3be9e7b827cd45bc9e2c00aedd72c81acbb6d8
younghoon <martin.younghoon.kim@gmail.com>
2017-05-30 17:14:15 +0900

    2017-05-30 김영훈 메인 페럴렉스 네비게이션 기능 추가

--- js/module/navigation.js
+++ js/module/navigation.js
@@ -1,33 +1,93 @@
 /*
 *********************************************************************************
     slide 작성
-    작성일 : 2017.04.24
-    작성자 : 이영주
+    작성일 : 2017.05.30
+    작성자 : 김영훈
     내용 : 메인 navigation 기능
 *********************************************************************************
 */
 define([
 	'jquery'
-	,'wheel'
-],function ($,wheel) {
-	var navigation = function(element){
-		this.wrap = $(element.wrap);
-		this.play= false;
-	}
-	navigation.prototype={
-		init:function(){
-			var that =this;
-			var scrollY = Math.max( document.body.scrollTop, document.documentElement.scrollTop);
-			var limit = 200;
-			//console.log(this.wrap.offset().top)
+],function ($) {
+	var navigation = (function(){
+		function init(options){
+			var wrapper = setSelector(options).wrapper;
+			var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+			var startPoint = getOffSetTop(options);
+			var currentPoint =getCurrentPosition(startPoint, scrollTop);
+
 			$(document).scroll( function(){
-				scrollY = Math.max( document.body.scrollTop, document.documentElement.scrollTop);
-				if(that.wrap.offset().top - scrollY <= limit ){
-					this.play =true;
-				}
+				scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+				currentPoint=getCurrentPosition(startPoint, scrollTop);
+				navigationAction(options, currentPoint);
+			});
+			navigationAction(options, currentPoint);
+			bindEvents(options);
+		}
+
+		function setSelector(selector){
+			return {
+				"wrapper" : $(selector.wrapper),
+				"contents" : $(selector.contents),
+				"tabNavigation" :$(selector.tabNavigation)
+			}
+		}
+
+		function bindEvents(selector){
+			var navigationWrapper = setSelector(selector).tabNavigation;
+			var navigationButton = navigationWrapper.find("a");
+			navigationButton.on("click", function(evt){
+				evt.preventDefault();
+				scrollPosition($(this));
 			})
 		}
-	}
+
+		function getOffSetTop(selector){
+			var contents = setSelector(selector).contents;
+			var arr = [];
+			contents.each( function(idx){
+				arr.push($(this).offset().top)
+			})
+			return arr;
+		}
+
+		function getCurrentPosition(startPoint, scrollY){
+			var idx=0;
+			var i =0;
+			var limit = 100;
+			for(;i<startPoint.length;i++){
+				if(startPoint[i]-scrollY<=limit){
+					idx = i;
+				}
+			}
+			return idx;
+		}
+
+		function navigationAction(selector, point){
+			var tabNavigation = setSelector(selector).tabNavigation;
+			var tabList = setSelector(selector).tabNavigation.children("li");
+			var limit = tabList.size()-1;
+
+			if(point > limit){
+				point = limit;
+			}
+			tabList.removeClass("on");
+			tabList.eq(point).addClass("on");
+		}
+
+		function scrollPosition(me){
+			var getId = me.attr("href");
+			var position = $(getId).offset().top;
+			$('body, html').animate({
+				scrollTop : position
+			})
+
+		}
+
+		return{
+			init :init
+		}
+	})();
 	return navigation;
 
 });
